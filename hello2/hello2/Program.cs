5e0ed0187a0dee16e1bf5be9e0adbca3f6385a4e
Danilo <danilofes@gmail.com>
2015-03-06 21:58:31 -0300

    Pequena mudança

--- hello2/hello2/Program.cs
+++ hello2/hello2/Program.cs
@@ -11,6 +11,8 @@ namespace hello2
         static void Main(string[] args)
         {
             Console.WriteLine("hello world!");
+
+            Console.WriteLine("hello world!");
         }
     }
 }
