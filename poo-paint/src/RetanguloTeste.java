58b5dad5ba9b2a3ff9e3dbbafd5a9a357393fc90
Danilo <danilofes@gmail.com>
2015-03-09 21:26:00 -0300

    Teste unitário da prática 3.

--- poo-paint/src/RetanguloTeste.java
+++ poo-paint/src/RetanguloTeste.java
@@ -13,4 +13,16 @@ public class RetanguloTeste {
 		Assert.assertEquals("retangulo[X:31,Y:31,l:40,a:50]", saida);
 	}
 
+	@Test
+	public void testaContadorDeInstancias() {
+		Assert.assertEquals(0, Retangulo.instancias());
+		
+		Retangulo r1 = new Retangulo(31, 31, 50, 40);
+		Retangulo r2 = new Retangulo(15, 45, 52, 40);
+		Assert.assertEquals(2, Retangulo.instancias());
+		
+		Retangulo r3 = new Retangulo(15, 45, 52, 40);
+		Assert.assertEquals(3, Retangulo.instancias());
+	}
+
 }
