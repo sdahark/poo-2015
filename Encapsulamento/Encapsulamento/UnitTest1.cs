e90624d660f800ad1702b0066c9011a52c9267c9
Danilo F. Silva <danilofes@gmail.com>
2015-03-10 19:52:23 -0300

    Encapsulamento.

---
+++ Encapsulamento/Encapsulamento/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Encapsulamento
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
+
+namespace ModuloB
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
