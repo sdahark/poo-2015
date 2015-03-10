743d1559dcae26cf1510f821ab3a0a1a41399053
Danilo <danilofes@gmail.com>
2015-03-09 22:16:34 -0300

    Teste do método igual.

--- poo-paint/src/RetanguloTeste.java
+++ poo-paint/src/RetanguloTeste.java
@@ -15,6 +15,7 @@ public class RetanguloTeste {
 
 	@Test
 	public void testaContadorDeInstancias() {
+		Retangulo.zeraInstancias();
 		Assert.assertEquals(0, Retangulo.instancias());
 		
 		Retangulo r1 = new Retangulo(31, 31, 50, 40);
@@ -23,6 +24,17 @@ public class RetanguloTeste {
 		
 		Retangulo r3 = new Retangulo(15, 45, 52, 40);
 		Assert.assertEquals(3, Retangulo.instancias());
+	}
+
+	@Test
+	public void testaIgual() {
+		Retangulo r1 = new Retangulo(31, 31, 50, 40);
+		Retangulo r2 = new Retangulo(15, 45, 52, 40);
+		Assert.assertEquals(false, Retangulo.igual(r1, r2));
+		
+		Retangulo r3 = new Retangulo(31, 31, 50, 40);
+		Retangulo r4 = new Retangulo(31, 31, 50, 40);
+		Assert.assertEquals(true, Retangulo.igual(r3, r4));
 	}
 
 }
