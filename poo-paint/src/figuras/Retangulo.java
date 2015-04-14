796b4d5468e29093c25222f1b74512c210bb5d8a
João Vitor <sdshark@hotmail.com>
2015-04-13 22:22:00 -0300

    AULA 13/04

--- poo-paint/src/figuras/Retangulo.java
+++ poo-paint/src/figuras/Retangulo.java
@@ -1,27 +1,20 @@
 package figuras;
-public class Retangulo {
 
-	private int x;
-	private int y;
+import java.awt.Graphics;
+
+public class Retangulo extends Figura {
+
 	private int altura;
 	private int largura;
-	private static int cont = 0;
 	
-	
-	public int getX() {
-		return x;
+	public Retangulo(int x, int y, int largura, int altura) {
+		super(x, y);
+		this.altura = altura;
+		this.largura = largura;
 	}
 
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
+	public String imprime() {
+		return "retangulo[X:"+this.x+",Y:"+this.y+",l:"+this.largura+",a:"+this.altura+"]";
 	}
 
 	public int getAltura() {
@@ -31,28 +24,9 @@ public class Retangulo {
 	public int getLargura() {
 		return largura;
 	}
-
-	public Retangulo(int x, int y, int altura, int largura) {
-		this.x = x;
-		this.y = y;
-		this.altura = altura;
-		this.largura = largura;
-		cont ++;
-		
-	}
-
-	public String imprime() {
-		return "retangulo[X:"+this.x+",Y:"+this.y+",l:"+this.largura+",a:"+this.altura+"]";
-	}
-	public static void zerainstancias(){
-		cont = 0;		
-	}
-	public static int instancias(){
-		return cont;
-	}
-
-	public static boolean igual(Retangulo r1, Retangulo r2) {	
-		return (r1.x == r2.x && r1.y == r2.y && r1.altura == r2.altura && r1.largura == r2.largura);
+	@Override
+	public void Desenha (Graphics g){
+		g.drawRect(400, 400, 200, 250);
 	}
 
 }
--- poo-paint/src/testes/RetanguloTeste.java
+++ poo-paint/src/testes/RetanguloTeste.java
@@ -2,39 +2,38 @@ package testes;
 import org.junit.Assert;
 import org.junit.Test;
 
+import figuras.Circulo;
+import figuras.Figura;
 import figuras.Retangulo;
 
 
 public class RetanguloTeste {
+
 	@Test
 	public void testaImprime() {
-		Retangulo r = new Retangulo(31, 31, 50, 40);
+		Retangulo r = new Retangulo(31, 31, 40, 50);
 		
 		String saida = r.imprime();
-	    
 		Assert.assertEquals("retangulo[X:31,Y:31,l:40,a:50]", saida);
+		
+		Retangulo r2 = new Retangulo(14, 9, 35, 67);
+		Assert.assertEquals("retangulo[X:14,Y:9,l:35,a:67]", r2.imprime());
 	}
 
-	@Test
-	public void testaContadorDeInstancias() {
-		Retangulo.zerainstancias();
-		Assert.assertEquals(0, Retangulo.instancias());
-		
-		Retangulo r1 = new Retangulo(31, 31, 50, 40);
-		Retangulo r2 = new Retangulo(15, 45, 52, 40);
-		Assert.assertEquals(2, Retangulo.instancias());
-		
-		Retangulo r3 = new Retangulo(15, 45, 52, 40);
-		Assert.assertEquals(3, Retangulo.instancias());
-	}	
-	public void testaIgual() {
-		Retangulo r1 = new Retangulo(31, 31, 50, 40);
-		Retangulo r2 = new Retangulo(15, 45, 52, 40);
-		Assert.assertEquals(false, Retangulo.igual(r1, r2));
-		
-		Retangulo r3 = new Retangulo(31, 31, 50, 40);
-		Retangulo r4 = new Retangulo(31, 31, 50, 40);
-		Assert.assertEquals(true, Retangulo.igual(r3, r4));
 
-}
+	@Test
+	public void testaHerança() {
+		Retangulo r = new Retangulo(31, 31, 40, 50);
+		Circulo c = new Circulo (31, 56, 20);
+		
+		Figura f1 = r;
+		Figura f2 = c;
+		
+		String saida = r.imprime();
+		Assert.assertEquals("retangulo[X:31,Y:31,l:40,a:50]", saida);
+		
+		Retangulo r2 = new Retangulo(14, 9, 35, 67);
+		Assert.assertEquals("retangulo[X:14,Y:9,l:35,a:67]", r2.imprime());
+	}
+	
 }
